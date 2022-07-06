07704410eca123dff1748d01dc6cd5fc4fa6814b
vishalkumar-activelabs <vishal.kumar@cloudactivelabs.com>
2022-07-06 18:52:34 +0530

    terminate-queue-using-fetch

--- index.js
+++ index.js
@@ -1,66 +1,72 @@
-const PgBoss = require('pg-boss');
-const dotenv = require('dotenv');
+const PgBoss = require("pg-boss");
+const dotenv = require("dotenv");
 dotenv.config();
 
 const dbURl = `${process.env.DATABASE}://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DB_NAME}`;
-console.log({dbURl});
+console.log({ dbURl });
 
-/*      Connecting PgBoss to postres        */ 
+let breakOnGoingQueue = false;
+/*      Connecting PgBoss to postres        */
 const boss = new PgBoss(dbURl);
 // const boss = new PgBoss('postgres://firstuser:mypass@localhost:5433/mynewdb');
 
+const queue = "email-daily-digest";
+const batchSize = 20;
 const ordersPayloadData = [
-    {
-        email: "billybob1@veganplumbing.com",
-        name: "Billy Bob1"
-    },
-    {
-        email: "billybob2@veganplumbing.com",
-        name: "Billy Bob2"
-    },
-    {
-        email: "billybob3@veganplumbing.com",
-        name: "Billy Bob3"
-    },
-    {
-        email: "billybob4@veganplumbing.com",
-        name: "Billy Bob4"
-    },
-    {
-        email: "billybob5@veganplumbing.com",
-        name: "Billy Bob5"
-    },
-]
-const queue = 'Pizza order queue';
+  {
+    email: "billybob1@veganplumbing.com",
+    name: "Billy Bob1",
+  },
+  {
+    email: "billybob2@veganplumbing.com",
+    name: "Billy Bob2",
+  },
+  {
+    email: "billybob3@veganplumbing.com",
+    name: "Billy Bob3",
+  },
+  {
+    email: "billybob4@veganplumbing.com",
+    name: "Billy Bob4",
+  },
+  {
+    email: "billybob5@veganplumbing.com",
+    name: "Billy Bob5",
+  },
+];
 
-boss.on('monitor-states',(success, error) => console.log('monitor-states',success,error))
-// boss.on('wip',(success, error) => console.log('wip',success,error))
-boss.on('stopped',(success, error) => console.log('stopped',success,error))
-
-boss.on('error', error => console.error('boss raised error',{error}));
-boss.on('connected', (success, error) => console.log('connected',success,error));
-boss.start('monitor-states', (success, error) => console.log('started',success,error));
-
-let workid = ''
-
-
-const pizzaOrderQueue = async() => {
-    try {
+const pizzaOrderQueue = async () => {
+  try {
     await boss.start();
-        console.log({ queue, ordersPayloadData })
-        for(let order of ordersPayloadData){
-            const jobId = await boss.send(queue, order)
-            console.log('54', { jobId })
-        }
-        workid = await boss.work(queue, { 
-            teamSize: 15,
-            // batchSize: 5 ,
-        },someAsyncJobHandler)
-            
-    } catch (error) {
-        console.log('61->',{error})
+    for (let order of ordersPayloadData) {
+      const jobId = await boss.send(queue, order);
+      console.log("54", { jobId });
     }
-}
+    const jobs = await boss.fetch(queue, batchSize);
+    console.log({ jobs });
+    if (!jobs) {
+      console.log("No jobs found");
+      return;
+    }
+    for (let i = 0; i < jobs.length; i++) {
+        const job = jobs[i]
+    if(breakOnGoingQueue){
+        console.log("Queue is breaked now.")
+        break;
+    }
+        try {
+            await someAsyncJobHandler(job)
+            await boss.complete(job.id)
+        } catch(err) {
+            await boss.fail(job.id, err)
+        }
+    }
+  } catch (error) {
+    console.log("61->", { error });
+  }
+};
+pizzaOrderQueue();
+
 const someAsyncJobHandler = async(job) => {
 
     console.log(`job ${job.id} received with data:`);
@@ -72,15 +78,19 @@ const someAsyncJobHandler = async(job) => {
 const sleep =(ms) =>{
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-pizzaOrderQueue();
 
-process.on('SIGTERM', () => {
-    console.info('SIGTERM signal received. Request found to kill current server --');
-    boss.offWork({id: workid})
-    // boss.stop({graceful: false})
-});
-process.on('SIGINT', () => {
+// process.on('SIGTERM', () => {
+//     console.info('SIGTERM signal received. Request found to kill current server --');
+//     // boss.offWork({id: workid})
+//     // boss.stop({graceful: false})
+// });
+process.on('SIGINT', async() => {
     console.info('SIGKILL signal received. Request found to kill current server --');
-    boss.offWork({id: workid})
-    boss.stop({graceful: false})
+    // await boss.offWork(queue)
+    // const qsiez = await boss.getQueueSize(queue)
+
+    // console.log({qsiez})
+    breakOnGoingQueue = true;
+    // await boss.stop()
+    // await boss.clearStorage()
 });
